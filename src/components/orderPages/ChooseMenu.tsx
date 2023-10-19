58c50dcd2b113270d2543ff0f9ca13e680f74cd7
wmjin9312 <wmjin9312@gmail.com>
2023-10-19 11:05:44 +0900

    Menu logic

--- src/components/orderPages/ChooseMenu.tsx
+++ src/components/orderPages/ChooseMenu.tsx
@@ -4,15 +4,33 @@ import axios from 'axios';
 import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
 
+
 export default function ChooseMenu() {
 
   const [data, setData] = useState([]);
   const [selected, setSelected] = useState([]);
+  const [totalPrice, setTotalPrice] = useState(0);
 
 
   const toggleSelected = (item) => {
-    setSelected(item.price)
+    setSelected((current) => {
+      const newSelected = [...current];
+      const selectedItem = newSelected.find((selectItem) => selectItem.id === item.id);
 
+      if (selectedItem) {
+        
+        selectedItem.quantity++;
+        setTotalPrice(prevTotalPrice => prevTotalPrice + selectedItem.price)
+      } else {
+        
+        newSelected.push({ ...item, quantity: 1, dishId:item.id });
+        setTotalPrice(prevTotalPrice => prevTotalPrice + item.price)
+      }
+      
+
+
+      return newSelected;
+    });
   }
 
   const fetchData = async () => {
@@ -28,6 +46,8 @@ export default function ChooseMenu() {
     fetchData();
   },[])
 
+
+
   return (
     <div>
       <h3>메뉴 선택</h3>
@@ -37,14 +57,20 @@ export default function ChooseMenu() {
             <Button
               key={item.id}
               variant={'outlined'}
-              onClick={() => { toggleSelected(item.price) }}> 
+              onClick={() => { toggleSelected(item) }}> 
               {item.name}<br /> ${item.price}
             </Button>
           )) 
         }
       </Stack>
 
-      <h3>{selected}</h3>
+      <div>{totalPrice}</div>
+      <div>- soup의 개수 : x </div>
+      <div>- salad의 개수 : x </div>
+      <div>- steak의 개수 : x </div>
+      <div>- 총 금액 : {totalPrice} </div>
+
+
 
       <Stack direction= "row" alignItems="center" justifyContent="center">
         <Link to='/ChooseCook'>
